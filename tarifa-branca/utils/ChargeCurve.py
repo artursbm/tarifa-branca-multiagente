fe5b4f28918cfe75b34a0a77994f0eedb4ad4a59
Artur Mello <artursbm@github.com>
2019-08-26 23:20:06 -0300

    progress on making consumer profile

--- tarifa-branca/utils/ChargeCurve.py
+++ tarifa-branca/utils/ChargeCurve.py
@@ -1,42 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 class ChargeCurve:
     def __init__(self):
-        self.white_tariff = np.array([0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.76971, 0.76971,
-                                      1.19806, 1.19806,
-                                      1.19806, 1.19806,
-                                      1.19806, 1.19806,
-                                      0.76971, 0.76971,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894,
-                                      0.51894, 0.51894])
-        self.conventional_tariff = np.array([0.62833] * 25)
+        self.white_tariff = pd.DataFrame({"time": np.arange(0, 24, 0.5),
+                                          "value": np.array([0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.76971, 0.76971,
+                                                             1.19806, 1.19806,
+                                                             1.19806, 1.19806,
+                                                             1.19806, 1.19806,
+                                                             0.76971, 0.76971,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894,
+                                                             0.51894, 0.51894])})
+
+        self.conventional_tariff = pd.DataFrame({"time": np.arange(0, 24.5, 0.5),
+                                                 "value": np.array([0.62833] * 49)})
 
     def plot_charge_cost(self):
         fig = plt.figure()
         s = fig.add_subplot(111)
-        s.bar(np.arange(0.5, 24.5, 0.5), self.white_tariff)
-        s.plot(np.arange(0, 25, 1), self.conventional_tariff, color='red')
         s.set_xticks(range(0, 25))
+        s.bar(self.white_tariff['time'], self.white_tariff['value'],
+              align='edge',
+              color='green',
+              label='Tarifa Branca')
+        s.plot(self.conventional_tariff['time'], self.conventional_tariff['value'],
+               color='red',
+               label='Tarifa Convencional')
 
+        s.legend()
         plt.ylabel('R$/kWh')
         plt.xlabel('horário')
         plt.title("Custos de Tarifa Branca vs. Tarifa Convencional")
--- tarifa-branca/utils/ProfileGenerator.py
+++ tarifa-branca/utils/ProfileGenerator.py
@@ -1,5 +1,35 @@
+import random
+
+import pandas as pd
+
+
 def generate_profile(agent_id):
+    product_list = open_product_list()
+    consumer_products = create_consumer_products()
+
+    for product in product_list.get_values():
+        consumer_products.append(map_product_to_consumer(product))
+
     print("generating profile for agent " + str(agent_id))
 
 
+def open_product_list():
+    return pd.read_csv("./utils/aparelhos_selecionados.csv")
 
+
+def create_consumer_products():
+    # initialize empty consumer product profile with defined columns
+    return pd.DataFrame(["Produto", "Quantidade", "Consumo (kWh)", "timeOfUse"])
+
+
+def map_product_to_consumer(product):
+    return {
+        "Produto": product["Produto"],
+        "Quantidade": random.randrange(product["MaxQuant"]),
+        "Consumo (kWh)": product["Consumo (kWh)"],
+        "timeOfUse": chose_time_of_use(product["Intervalos de uso"], product["horasDia"])
+
+    }
+
+def chose_time_of_use(time_intervals):
+    
